6c453b8bdf202c41f25fa5049dcacfd86b4cad78
heitor <heitorleite.dev@gmail.com>
2020-10-19 11:34:49 -0300

    add domino piling solution

---
+++ a/50.cpp
@@ -0,0 +1,15 @@
+// 50A - Domino Piling
+
+#include <iostream>
+using namespace std;
+#define endl "\n"
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int m, n;
+  cin >> m >> n;
+
+  cout << (m*n)/2 << endl;
+}
